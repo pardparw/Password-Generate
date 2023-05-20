d0839815ca70bb107cf90fe4bb4030bb59b2629c
pardparw <87960461+pardparw@users.noreply.github.com>
2023-05-20 10:18:55 +0700

    Update main.cpp

--- Password.C++/main.cpp
+++ Password.C++/main.cpp
@@ -1,30 +1,25 @@
+#include <stdlib.h>
 #include <iostream>
-#include <fstream>
 #include <random>
-#include "nlohmann/json.hpp"
-#include <string.h>
+//mySql lib
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+
+//color
 #include "color.hpp"
-
-using json = nlohmann::json;
 using namespace std;
-void savePasswords(const json& j, const std::string& filename)
-{
-    std::ofstream file(filename);
-    file << std::setw(4) << j << std::endl;
-}
 
-json loadPasswords(const std::string& filename)
-{
-    std::ifstream file(filename);
-    json j;
-    file >> j;
-    return j;
-}
+//for demonstration only. never save your password in the code!
+const string server = "tcp://127.0.0.1:3306";
+const string username = "root";
+const string password = "password";
 
 std::string generatePassword(int length)
 {
     std::string password;
-    std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={}[]<>,.?/|";
+    std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&()_-+={}[]<>,.?/|";
     std::mt19937 rng(std::random_device{}());
     std::uniform_int_distribution<int> dist(0, characters.size() - 1);
 
@@ -35,80 +30,154 @@ std::string generatePassword(int length)
     return password;
 }
 
-void addPassword(json& j, const std::string& site, const std::string& username, const std::string& password)
-{
-    json password_obj = {
-        {"site", site},
-        {"username", username},
-        {"password", password}
-    };
-    j.push_back(password_obj);
-    std::cout << "Password saved successfully." << std::endl;
-}
 
-void editPassword(json& j, int id, const std::string& site, const std::string& username, const std::string& password)
-{
-    if (id >= 0 && id < j.size()) {
-        json& password_obj = j[id];
-        password_obj["site"] = site;
-        password_obj["username"] = username;
-        password_obj["password"] = password;
-        std::cout << "Password edited successfully." << std::endl;
-    }
-    else {
-        std::cerr << "Error: Invalid password ID." << std::endl;
-    }
-}
 
-void deletePassword(json& j, int id)
+void addPassword(const std::string& site, const std::string& usernames, const std::string& passwords)
 {
-    if (id >= 0 && id < j.size()) {
-        j.erase(j.begin() + id);
-        std::cout << "Password deleted successfully." << std::endl;
-    }
-    else {
-        std::cerr << "Error: Invalid password ID." << std::endl;
-    }
-}
+    //DB Setting
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::Statement* stmt;
+    sql::PreparedStatement* pstmt;
 
-void listPasswords(const json& j)
-{   
+    try
+    {
+        driver = get_driver_instance();
+        con = driver->connect(server, username, password);
+    }
+    catch (sql::SQLException e)
+    {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        system("pause");
+        exit(1);
+    }
+    //please create database "quickstartdb" ahead of time
+    con->setSchema("password_db");
     
-    if (!j.empty()) {
-        std::cout << "Saved passwords:" << std::endl;
-        for (int i = 0; i < j.size(); ++i) {
-            const auto& password = j[i];
-            std::cout << "[" << i << "] " << password["site"].get<std::string>() << std::endl;
-        }
-        
-    }
-    else {
-        std::cout << "No saved passwords found." << std::endl;
-    }
+    pstmt = con->prepareStatement("INSERT INTO passwords(site, username, PASSWORD) VALUES(?,?,?)");
+    pstmt->setString(1, site);
+    pstmt->setString(2 , usernames);
+    pstmt->setString(3, passwords);
+    pstmt->execute();
+    cout << "Add Sucess";
+    
 }
 
-void viewPassword(const json& j, int id)
+void editPassword(const std::string& id, const std::string& site, const std::string& usernames, const std::string& passwords)
 {
-    if (id >= 0 && id < j.size()) {
-        const auto& password = j[id];
-        std::cout << "Site: " << password["site"].get<std::string>() << std::endl;
-        std::cout << "Username: " << password["username"].get<std::string>() << std::endl;
-        std::cout << "Password: " << password["password"].get<std::string>() << std::endl;
+    //DB Setting
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::Statement* stmt;
+    sql::PreparedStatement* pstmt;
+
+    try
+    {
+        driver = get_driver_instance();
+        con = driver->connect(server, username, password);
     }
-    else {
-        std::cerr << "Error: Invalid password ID." << std::endl;
+    catch (sql::SQLException e)
+    {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        system("pause");
+        exit(1);
     }
+    //please create database "quickstartdb" ahead of time
+    con->setSchema("password_db");
+
+    pstmt = con->prepareStatement("UPDATE passwords SET site = ?, username = ?, PASSWORD = ? WHERE id = " + id);
+    
+    pstmt->setString(1, site);
+    pstmt->setString(2, usernames);
+    pstmt->setString(3, passwords);
+    pstmt->execute();
+    cout << "Update Sucess";
+}
+
+void deletePassword(const std::string& id)
+{
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::PreparedStatement* pstmt;
+    sql::ResultSet* result;
+
+    try
+    {
+        driver = get_driver_instance();
+        //for demonstration only. never save password in the code!
+        con = driver->connect(server, username, password);
+    }
+    catch (sql::SQLException e)
+    {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        system("pause");
+        exit(1);
+    }
+
+    con->setSchema("password_db");
+
+    //delete
+    pstmt = con->prepareStatement("DELETE FROM passwords WHERE id = ?");
+    pstmt->setString(1, id);
+    result = pstmt->executeQuery();
+    //Update ID
+    pstmt = con->prepareStatement("SET @autoid :=0");
+    pstmt->execute();
+    pstmt = con->prepareStatement("UPDATE passwords SET id = @autoid := (@autoid+1)");
+    pstmt->execute();
+    pstmt = con->prepareStatement("alter TABLE passwords Auto_increment = 1");
+    pstmt->execute();
+    //clear Deleat data
+    delete pstmt;
+    delete con;
+    delete result;
+    cout << "Deleat Sucess" << endl;
+}
+
+void listPasswords()
+{
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::PreparedStatement* pstmt;
+    sql::ResultSet* result;
+
+    try
+    {
+        driver = get_driver_instance();
+        //for demonstration only. never save password in the code!
+        con = driver->connect(server, username, password);
+    }
+    catch (sql::SQLException e)
+    {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        system("pause");
+        exit(1);
+    }
+
+    con->setSchema("password_db");
+
+    //select  
+    pstmt = con->prepareStatement("SELECT * FROM passwords;");
+    result = pstmt->executeQuery();
+    cout << dye::white_on_black("Id|site|\t|Username|\t|Password|") << endl;
+    while (result->next())
+        cout << dye::black_on_white("[" + result->getString(1) + "] " + result->getString(2) + "\t" + result->getString(3) + "\t" + result->getString(4)) << endl;
+        
+
+    delete result;
+    delete pstmt;
+    delete con;
 }
 
 int main()
 {
-    const std::string filename = "E:/Password/password/passwords.json";
-    json passwords = loadPasswords(filename);
+    //const std::string filename = "E:/Password/password/passwords.json";
+   // json passwords = loadPasswords(filename);
     string PasswordProgram;
     std::cout << "Welcome to Password Manager!" << std::endl;
     cout << "Input Password: ";
     cin >> PasswordProgram;
-    if (PasswordProgram == "youpassword")
+    if (PasswordProgram == "password")
     {
 
 
@@ -120,24 +189,24 @@ int main()
             std::cout << "3. Edit password" << std::endl;
             std::cout << "4. Delete password" << std::endl;
             std::cout << "5. List passwords" << std::endl;
-            std::cout << "6. View password" << std::endl;
             std::cout << "0. Quit" << std::endl;
 
             int choice;
             std::cout << "> ";
             std::cin >> choice;
+            
 
             if (choice == 0) {
                 break;
             }
-            else if (choice == 1) {
+            else if (choice == 1) {//RandomPassword
                 int length;
                 std::cout << "Enter password length: ";
                 std::cin >> length;
                 std::string password = generatePassword(length);
                 std::cout << "Generated password: " << password << std::endl;
             }
-            else if (choice == 2) {
+            else if (choice == 2) {//Add Password
                 std::string site, username, password;
                 std::cout << "Enter site name: ";
                 std::cin >> site;
@@ -145,11 +214,10 @@ int main()
                 std::cin >> username;
                 std::cout << "Enter password: ";
                 std::cin >> password;
-                addPassword(passwords, site, username, password);
-                savePasswords(passwords, filename);
+                addPassword(site, username, password);
             }
-            else if (choice == 3) {
-                int id;
+            else if (choice == 3) {//Edit Password
+                string id;
                 std::cout << "Enter password ID: ";
                 std::cin >> id;
                 std::string site, username, password;
@@ -159,25 +227,20 @@ int main()
                 std::cin >> username;
                 std::cout << "Enter password: ";
                 std::cin >> password;
-                editPassword(passwords, id, site, username, password);
-                savePasswords(passwords, filename);
+                editPassword(id, site, username, password);
+               
             }
-            else if (choice == 4) {
-                int id;
+            else if (choice == 4) {//Deleat Password
+                string id;
                 std::cout << "Enter password ID: ";
                 std::cin >> id;
-                deletePassword(passwords, id);
-                savePasswords(passwords, filename);
+                deletePassword(id);
+                
             }
             else if (choice == 5) {
-                listPasswords(passwords);
+                listPasswords();
             }
-            else if (choice == 6) {
-                int id;
-                std::cout << "Enter password ID: ";
-                std::cin >> id;
-                viewPassword(passwords, id);
-            }
+           
             else {
                 std::cerr << "Error: Invalid choice." << std::endl;
             }
@@ -186,10 +249,12 @@ int main()
     else
     {
         cout << dye::red("wrong Password") << endl;
-        cout << "Press [ctrl + C] to close this window . . .";
-        cin >> passwords;
+        cout << "Press [ctrl + C] to close this window . . ."<<endl;
+        //cin >> passwords;
     }
     std::cout << "Goodbye!" << std::endl;
 
     return 0;
 }
+
+
